76477377abbddefdb55c6d097295a7a710a67aa8
Md-Tur <turibnmostafiz@gmail.com>
2022-02-11 07:20:47 +0800

    parameter & variable name fixed

--- js/banking.js
+++ js/banking.js
@@ -1,22 +1,22 @@
 // deposit part including btn & input
 
-function getInputValue() {
-    const depostiInput = document.getElementById('deposit-input');
-    const depositAmountText = depostiInput.value;
-    const depositAmount = parseFloat(depositAmountText);
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmount = inputField.value;
+    const totalAmount = parseFloat(inputAmount);
 
     // clear deposit input field
 
-    depostiInput.value = '';
+    inputField.value = '';
 
-    return depositAmount;
+    return totalAmount;
 }
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     /* const depostiInput = document.getElementById('deposit-input');
     const depositAmountText = depostiInput.value;
     const depositAmount = parseFloat(depositAmountText); */
-    const depositAmount = getInputValue();
+    const depositAmount = getInputValue('deposit-input');
 
     // get current deposit
 
@@ -37,9 +37,10 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 // withdraw part including btn & input
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawAmountText = withdrawInput.value;
-    const withdrawAmount = parseFloat(withdrawAmountText);
+    /*     const withdrawInput = document.getElementById('withdraw-input');
+        const withdrawAmountText = withdrawInput.value;
+        const withdrawAmount = parseFloat(withdrawAmountText); */
+    const withdrawAmount = getInputValue('withdraw-input');
 
     // update withdraw total
 
@@ -54,8 +55,8 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
     balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
-
-    // clear withdraw input field
-
-    withdrawInput.value = '';
+    /* 
+        // clear withdraw input field
+    
+        withdrawInput.value = ''; */
 })
